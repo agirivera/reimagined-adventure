29a5d3d37113a9ca27b1c6dccb9347395a7110f5
Alec Joseph Rivera <agirivera+dev@gmail.com>
2019-07-27 17:45:38 +0800

    Add listing 02-03 as is from the book

---
+++ list0203.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <limits>
+#include <ostream>
+
+int main()
+{
+    // Note that "digits" means binary digits, i.e., bits.
+    std::cout << "bits per bool: " << std::numeric_limits<bool>::digits << '\n';
+}

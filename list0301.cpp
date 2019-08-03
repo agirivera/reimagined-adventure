6b059fe23b19e89273471a85cc76261d2d1b25ac
Alec Joseph Rivera <agirivera+dev@gmail.com>
2019-08-03 23:53:17 +0800

    Add listing 3-1 as is from the book

---
+++ list0301.cpp
@@ -0,0 +1,18 @@
+/// Read the program and determine what the program does.
+
+#include <iostream>
+#include <ostream>
+
+int main()
+{
+    int sum(0);
+    int count(0);
+    int x;
+    while (std::cin >> x)
+    {
+        sum = sum + x;
+        count = count + 1;
+    }
+
+    std::cout << "average = " << sum / count << '\n';
+}

f455312a8098e74f99bd3e5de8bfbcee326848df
Alec Joseph Rivera <agirivera+dev@gmail.com>
2019-08-04 00:08:44 +0800

    Add listing 03-03 as is from the book

---
+++ list0303.cpp
@@ -0,0 +1,16 @@
+/// Read integers and print a message that tells the user
+/// whether the number is even or odd.
+
+#include <iostream>
+#include <istream>
+#include <ostream>
+
+int main()
+{
+    int x;
+    while (std::cin >> x)
+        if ()  // Fill in the condition
+            std::cout << x << " is odd.\n";
+        else
+            std::cout << x << " is even.\n";
+}

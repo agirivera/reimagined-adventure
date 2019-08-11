68cc48dc1215eee899bec95e1513413e93133181
Alec Joseph Rivera <agirivera+dev@gmail.com>
2019-08-11 23:47:00 +0800

    Fill in loop

--- list0704.cpp
+++ list0704.cpp
@@ -5,7 +5,9 @@ int main()
 {
     int sum(0);
 
-    // Fill in the loop here.
+    for (int i(10); i <= 20; i = i + 1) {
+        sum = sum + i;
+    }
 
     std::cout << "Sum of 10 to 20 = " << sum << '\n';
 }

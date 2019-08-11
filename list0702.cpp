e8a329f601962f9cc0fb513cf39ddb4d535e9239
Alec Joseph Rivera <agirivera+dev@gmail.com>
2019-08-11 23:28:58 +0800

    Add listing 07-02 from book as is

---
+++ list0702.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <ostream>
+
+int main()
+{
+    for (int i(0); i != 10; i = i + 1)
+        std::cout << i << '\n';
+    std::cout << "i=" << i << '\n';     // error: i is undefined outside the loop
+}
